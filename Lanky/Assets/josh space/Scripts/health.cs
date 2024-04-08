cf815ca1183547cef7f88511d3c4269d1074db08
josh <1717734@mukilteo.wednet.edu>
2024-04-08 12:11:57 -0700

    started the heart health damage

---
+++ Lanky/Assets/josh space/Scripts/health.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class health : MonoBehaviour
+{
+
+    public int Health;
+    public int NumOfHearts;
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyheart;
+ 
+}
