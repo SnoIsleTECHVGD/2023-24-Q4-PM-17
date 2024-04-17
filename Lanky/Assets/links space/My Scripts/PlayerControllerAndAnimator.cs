7100633b48b23c49de8ec08dbedb66495c490c72
MUKILTEO\1717469 <1717469@mukilteo.wednet.edu>
2024-04-17 13:27:45 -0700

    making Lanky movement and animation script

--- Lanky/Assets/links space/My Scripts/PlayerControllerAndAnimator.cs
+++ Lanky/Assets/links space/My Scripts/PlayerControllerAndAnimator.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerControllerAndAnimator : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rb;
+    public float speed;
+    private float moveInput;
+    
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        moveInput = moveInput.GetAxisRaw("Horiontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
     }
 }
