82871e0b75a5d1bfb9cf5b9b040183fbd14c7a32
Mauya <1717854@mukilteo.wednet.edu>
2024-04-17 13:20:38 -0700

    more more

---
+++ Lanky/Assets/leos space/scripts/VolumeControll.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeControll : MonoBehaviour
+{
+   public AudioListener audioListener;
+    private bool audioOn;
+
+
+
+    public void AudioOn()
+    {
+
+    }
+
+
+}
