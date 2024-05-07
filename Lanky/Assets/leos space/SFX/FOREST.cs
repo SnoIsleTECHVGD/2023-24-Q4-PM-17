563c1ac282dbfe159d69d47562f81799dbc4a2e7
MUKILTEO\1717854 <1717854@mukilteo.wednet.edu>
2024-05-07 13:27:43 -0700

    more stuff

--- Lanky/Assets/leos space/SFX/FOREST.cs
+++ Lanky/Assets/leos space/SFX/FOREST.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FOREST : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void update()
     {
         AudioManager.Instance.MusicPLayer("FOREST");
     }
