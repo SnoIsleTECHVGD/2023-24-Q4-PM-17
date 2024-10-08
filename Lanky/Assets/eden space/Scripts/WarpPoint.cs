f08b4ed2167761261ed0440f6ff343735bac243a
1717854 <1717854@mukilteo.wednet.edu>
2024-10-08 13:26:18 -0700

    more more stuff, final scene.

--- Lanky/Assets/eden space/Scripts/WarpPoint.cs
+++ Lanky/Assets/eden space/Scripts/WarpPoint.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class WarpPoint : MonoBehaviour
 {
     public static bool ignorewarp = false;
-    public Vector2 Warp = new Vector2(-18.25f, 3.71f);
+    public Vector3 Warp = new Vector3(-18.25f, 3.71f);
 
     public GameObject player;
+    public GameObject cam1;
+    public GameObject cam2;
+    public GameObject cam3;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         player.transform.position = Warp;
+
+        if(CompareTag("portal1"))
+        {
+          cam1.SetActive(false);
+            cam2.SetActive(true);
+        }
         //SceneManager.LoadScene(RoomName);
     }
     void Start()
