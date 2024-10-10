14a2ba624116fb4c3e7969d29574a906519e60fe
1717854 <1717854@mukilteo.wednet.edu>
2024-10-10 13:26:03 -0700

    finish the damn final scene.

--- Lanky/Assets/eden space/Scripts/PLatform.cs
+++ Lanky/Assets/eden space/Scripts/PLatform.cs
@@ -10,31 +10,44 @@ public class PLatform : MonoBehaviour
     private Animator anima;
     private Transform currentPoint;
     public float speed;
+    public bool reachedPoint = false;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        anima = GetComponent<Animator>();
+        //anima = GetComponent<Animator>();
         currentPoint = pointb.transform;
-        anima.SetBool("isRunning", true);
+        //anima.SetBool("isRunning", true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 point = currentPoint.position - transform.position;
+        //Vector2 point = currentPoint.position - transform.position;
+
+       //if(!reachedPoint)
+        {
+
+         //transform.position = Vector3.Lerp(transform.position, PointA.transform.position, speed);
+
+        }
+  
+        //if(Vector3.Distance(transform.position,PointA.transform.position) <= 0.1)
+        {
+           // reachedPoint = true;
+           // StartCoroutine(WaitTime(8));
+        }
 
         if (currentPoint == pointb.transform)
         {
-            rb.velocity = new Vector2( 0, speed);
+            rb.velocity = new Vector2(0,0);
         }
         else
         {
-            rb.velocity = new Vector2(0, -speed);
+            rb.velocity = new Vector2(0,speed);
         }
 
         if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointb.transform)
         {
-            Flip();
             currentPoint = PointA.transform;
         }
         if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == PointA.transform)
@@ -57,6 +70,18 @@ public class PLatform : MonoBehaviour
         Gizmos.DrawWireSphere(pointb.transform.position, 0.5f);
 
         Gizmos.DrawLine(pointb.transform.position, PointA.transform.position);
+    }
+
+
+
+   IEnumerator WaitTime(float secs)
+    {
+        yield return new WaitForSeconds(secs);
+        transform.position = Vector2.Lerp(transform.position,pointb.transform.position, speed*Time.deltaTime);
+        if(Vector2.Distance(transform.position, pointb.transform.position) <= 0.1)
+        {
+            reachedPoint = false;
+        }
     }
 }
 
